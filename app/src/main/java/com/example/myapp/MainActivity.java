a52248336273f286eb4b9eacd1334bdbd38a7abe
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2019-09-13 19:03:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArFragment arFragment=(ArFragment) getSupportFragmentManager().findFragmentById(R.id.app);
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            Anchor anchor=hitResult.createAnchor();
+
+            MaterialFactory.makeOpaqueWithColor(this,new Color(android.graphics.Color.RED))
+           .thenAccept(material -> {
+               ModelRenderable renderable= ShapeFactory.makeSphere(1.0f,new Vector3(0f,1f,1f),material);
+
+               AnchorNode anchorNode=new AnchorNode(anchor);
+               anchorNode.setRenderable(renderable);
+               arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+           });
+
+
+
+
+        });
+
+
+
+
+
+
+    }
+}
